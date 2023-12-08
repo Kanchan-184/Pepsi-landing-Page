773b73300ee1d1b7875755abe7a92332a59ffdef
Kanchan-184 <97331049+Kanchan-184@users.noreply.github.com>
2023-12-08 13:24:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgSlider(e) {
+    document.querySelector('.pepsi').src = e;
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.backgroundColor = color;
+}
